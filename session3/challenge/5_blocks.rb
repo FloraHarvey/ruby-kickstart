8ce0d7d77717d64c0bdafcfcb3859f66903c625a
Flora Harvey <fharvey555@gmail.com>
2017-01-28 19:40:41 +0000

    Attempt Ex5 - come back to

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -16,3 +16,24 @@
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+def spiral_access(array, &block)
+    num_rows_index = array.length - 1
+    num_columns_index = array[0].length - 1
+
+    array[0].each(&block)
+    array[1..(num_rows_index)].each do
+      |row| row[-1].each(&block)
+      end
+
+    array[num_rows_index]
+
+
+
+end
+
+# call block in order:
+# array[0].each
+# array[1][-1]
+# array[2][-1] etc... array[n][-1]
+# array[n][[-3..-2]
