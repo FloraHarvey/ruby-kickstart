6e8f61bdbcb339b5ccb2092aa84ad636c07d87cb
Flora Harvey <fharvey555@gmail.com>
2017-01-31 17:11:30 +0000

    Ex 11 solved

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -19,11 +19,52 @@
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
 
-num = 99
-for n in 0..98
-  puts "#{num} bottles of beer on the wall,"
-  puts "#{num} bottles of beer,"
-  puts "Take one down, pass it around,"
-  puts "#{num - 1} bottles of beer on the wall."
-  num -= 1
+
+class BeerSong
+
+  attr_accessor :number
+
+  def initialize (number)
+    if number < 0
+      number = 0
+    elsif number > 99
+      number = 99
+    end
+    self.number = number
+  end
+
+  def print_song
+    number.downto 1 do |i|
+      print_verse i
+    end
+  end
+
+  def print_verse(n)
+    if n.zero?
+      ""
+    else
+      puts "#{written_number n} #{sing_or_plural n} of beer on the wall,",
+           "#{written_number n} #{sing_or_plural n} of beer,",
+           "Take one down, pass it around,",
+           "#{written_number n - 1} #{sing_or_plural n-1} of beer on the wall."
+    end
+  end
+
+
+  def sing_or_plural(n)
+    if n == 1
+      'bottle'
+    else 'bottles'
+    end
+  end
+
+  def written_number(n)
+    if n >= 0 && n <= 19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[n]
+    elsif n % 10 == 0
+      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[n/10]
+    else
+      "#{written_number n/10*10}-#{written_number n%10}"
+    end.capitalize
+  end
 end
